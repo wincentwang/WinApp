85e9d9d3501d30a12722346889a3553a370b9ad2
Wincent.Wang <cunix24@sina.com>
2017-09-08 17:27:25 +0800

    - data dict

--- winapp-web/src/main/webapp/static/js/modules/base/dict.js
+++ winapp-web/src/main/webapp/static/js/modules/base/dict.js
@@ -7,6 +7,23 @@ $(function () {
 	getGrid();
 });
 
+
+var setting = {
+    data : {
+        simpleData : {
+            enable : true,
+            idKey : "dictId",
+            pIdKey : "parentId",
+            rootPId : -1
+        },
+        key : {
+            url : "nourl"
+        }
+    }
+};
+var ztree;
+
+
 function initialPage() {
 	$(window).resize(function() {
 	//	TreeGrid.table.resetHeight({height: $(window).height()-100});
@@ -28,21 +45,45 @@ function getGrid() {
 
 var vm = new Vue({
 	el:'#WinApp',
+	data:{
+        dict:{
+            parentName: null,
+            parentId: 0,
+            type: 1,
+            orderNum: 0,
+            status: 1
+        }
+	},
 	methods : {
 		load: function() {
 			TreeGrid.table.refresh();
 		},
 		save: function() {
-			dialogOpen({
-				title: '新增字典',
-				url: 'base/dict/add.html?_' + $.now(),
-				width: '600px',
-				height: '420px',
-				scroll : true,
-				yes : function(iframeId) {
-					top.frames[iframeId].vm.acceptClick();
-				},
-			});
+            layer.open({
+                type: 1,
+                offset: '50px',
+                title: "增加",
+                area: ['600px', '550px'],
+                shade: 0,
+                shadeClose: false,
+                content:$("#saveUpdateLayer"),
+                btn: ['确定', '取消'],
+                btn1: function (index) {
+                    vm.saveOrUpdate();
+                    layer.close(index);
+                }
+            });
+            this.getDict();
+			// dialogOpen({
+			// 	title: '新增字典',
+			// 	content:$("#saveUpdateForm"),
+			// 	width: '600px',
+			// 	height: '420px',
+			// 	scroll : true,
+			// 	yes : function(iframeId) {
+			// 		top.frames[iframeId].vm.acceptClick();
+			// 	},
+			// });
 		},
 		edit: function() {
 			var ck = TreeGrid.table.getSelectedRow();
@@ -54,7 +95,7 @@ var vm = new Vue({
 					height: '420px',
 					scroll : true,
 					success: function(iframeId){
-						top.frames[iframeId].vm.macro.macroId = ck[0].id;
+						top.frames[iframeId].vm.dict.dictId = ck[0].id;
 						top.frames[iframeId].vm.setForm();
 					},
 					yes : function(iframeId) {
@@ -70,14 +111,61 @@ var vm = new Vue({
 					ids[idx] = item.id;
 				});
 				$.RemoveForm({
-					url: '/sys/macro/remove?_' + $.now(),
+					url: '/sys/dict/remove?_' + $.now(),
 			    	param: ids,
 			    	success: function(data) {
 			    		vm.load();
 			    	}
 				});
 			}
-		}
+		},
+        dictTree: function(){
+            layer.open({
+                type: 1,
+                offset: '50px',
+                skin: 'layui-layer-molv',
+                title: "选择目录",
+                area: ['300px', '450px'],
+                shade: 0,
+                shadeClose: false,
+                content: jQuery("#dictTree"),
+                btn: ['确定', '取消'],
+                btn1: function (index) {
+                    var node = ztreeSelect.getSelectedNodes();
+                    //选择上级部门
+                    vm.dict.dictId = node[0].deptId;
+                    vm.dict.name = node[0].name;
+                    layer.close(index);
+                }
+            });
+        },
+        saveUpdate: function() {
+            if (!$('#form').Validform()) {
+                return false;
+            }
+            $.SaveForm({
+                url: '/sys/dict/save?_' + $.now(),
+                param: vm.dict,
+                success: function(data) {
+                    $.currentIframe().vm.load();
+                }
+            });
+        },
+        getDict : function() {
+            $.get('/sys/dict/select', function(r) {
+                ztree = $.fn.zTree
+                    .init($("#dictTree"), setting, r);
+                var node = ztree.getNodeByParam("dictId", vm.dict.parentId);
+                ztree.selectNode(node);
+                //vm.dict.typeName = node.name;
+            })
+        },
+        acceptClick: function() {
+            var node = ztree.getSelectedNodes();
+            top.layerForm.vm.macro.typeId = node[0].dictId;
+            top.layerForm.vm.macro.typeName = node[0].name;
+            dialogClose();
+        }
 	}
 })
 
